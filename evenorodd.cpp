6406aa539e22bbbbd815957171f49bdb5d9fc6e6
PRAJETHAJASMINE <113875279+PRAJETHAJASMINE@users.noreply.github.com>
2023-09-11 09:37:37 +0530

    Add files via upload

---
+++ evenorodd.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter an integer: ");
+	scanf("%d",&a);
+	if(a%2==0){
+		printf("the given input is even num ");
+	}
+	else{
+		printf("it is a odd number");
+	}
+}
